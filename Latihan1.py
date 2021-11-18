4ee612499bdd98af69ecc7f76084fbfbeaf22c7d
RaihanTantowi <raihantantowi9@gmail.com>
2021-11-18 23:31:44 +0700

    Lab2(Struktur kondisi)

---
+++ Latihan1.py
@@ -0,0 +1,12 @@
+#Membuat judul program
+print("="*86)
+print("Program sederhana untuk menentukan bilangan terbesar dari dua bilangan yang diinputkan")
+print("="*86)
+bil1=int(input("Masukan bilangan pertama= "))
+bil2=int(input("Masukan bilangan kedua= "))
+
+#Nilai Terbesar
+if (bil1>bil2):
+     print("Hasil bilangan terbesarnya adalah= ",bil1)
+elif (bil2>bil1):
+    print("Hasil bilangan terbesarnya adalah= ",bil2)
